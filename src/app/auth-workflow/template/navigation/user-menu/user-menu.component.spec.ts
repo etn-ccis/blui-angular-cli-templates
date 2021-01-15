0c80a32170f4f6cbadeeffa8b53c2860f612066f
Firm-Soft <evan.w.mclaughlin@gmail.com>
2021-01-15 14:31:11 -0500

    Fix unit test

--- src/app/auth-workflow/template/navigation/user-menu/user-menu.component.spec.ts
+++ src/app/auth-workflow/template/navigation/user-menu/user-menu.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserMenuComponent } from './user-menu.component';
+import {AppModule} from "../../app.module";
 
 describe('UserMenuComponent', () => {
     let component: UserMenuComponent;
@@ -8,7 +8,7 @@ describe('UserMenuComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [UserMenuComponent],
+            imports: [AppModule],
         }).compileComponents();
     });
 
