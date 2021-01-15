90dca722ed9f12c14ee0b12714b1a2623ae1ecf1
Firm-Soft <evan.w.mclaughlin@gmail.com>
2021-01-15 14:31:23 -0500

    Run prettier

--- src/app/auth-workflow/template/navigation/user-menu/user-menu.component.spec.ts
+++ src/app/auth-workflow/template/navigation/user-menu/user-menu.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserMenuComponent } from './user-menu.component';
-import {AppModule} from "../../app.module";
+import { AppModule } from '../../app.module';
 
 describe('UserMenuComponent', () => {
     let component: UserMenuComponent;
