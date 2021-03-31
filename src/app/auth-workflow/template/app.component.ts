45e7339951f345c711114ae8d8052526361ccc89
Firm-Soft <evan.w.mclaughlin@gmail.com>
2021-03-31 17:20:16 -0400

    Update changleog and auth workflow demo

--- src/app/auth-workflow/template/app.component.ts
+++ src/app/auth-workflow/template/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { LocalStorageService } from './services/auth-workflow/local-storage/local-storage.service';
+import { PxbAuthConfig, PxbAuthSecurityService, SecurityContext } from '@pxblue/angular-auth-workflow';
 
 @Component({
     selector: 'app-root',
     template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {
-    constructor(localStorageService: LocalStorageService) {
-        localStorageService.listenForAuthLoadingStateChanges();
+    constructor(
+        private readonly _pxbSecurityService: PxbAuthSecurityService,
+        private readonly _localStorageService: LocalStorageService,
+        private readonly _pxbAuthConfig: PxbAuthConfig
+    ) {
+        this._configurePxbAuthModule();
+        this._listenForAuthStateChanges();
+    }
+
+    private _configurePxbAuthModule(): void {
+        this._pxbAuthConfig.projectImage = 'assets/auth-workflow/eaton_stacked_logo.png';
+        this._pxbAuthConfig.backgroundImage = 'assets/auth-workflow/background.svg';
+        this._pxbAuthConfig.allowDebugMode = true;
+        this._pxbSecurityService.inferOnAuthenticatedRoute('home');
+    }
+
+    // When a user transitions between being logged in / logged out, update session information.
+    // This demo app stores session information in localStorage, this is just as a proof-of-concept.
+    private _listenForAuthStateChanges(): void {
+        this._pxbSecurityService.securityStateChanges().subscribe((state: SecurityContext) => {
+            const email = state.rememberMeDetails.email;
+            const rememberMe = state.rememberMeDetails.rememberMe;
+            const isAuth = state.isAuthenticatedUser;
+            this._localStorageService.setAuthData(rememberMe ? email : undefined, isAuth);
+        });
     }
 }
