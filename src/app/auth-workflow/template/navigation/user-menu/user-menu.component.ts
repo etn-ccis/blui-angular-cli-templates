77efde0c67ff0b832cdeaba524590da926c2c58b
RaghavendranC-Eaton <RaghavendranC@eaton.com>
2023-07-05 23:06:33 +0530

    mobile logout issue is fixed

--- src/app/auth-workflow/template/navigation/user-menu/user-menu.component.ts
+++ src/app/auth-workflow/template/navigation/user-menu/user-menu.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import {
     AUTH_ROUTES,
     BluiAuthSecurityService,
-    BluiChangePasswordDialogService,
+    BluiChangePasswordDialogService, 
 } from '@brightlayer-ui/angular-auth-workflow';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { Router } from '@angular/router';
-
 @Component({
     selector: 'app-user-menu',
     templateUrl: './user-menu.component.html',
@@ -17,16 +17,19 @@ export class UserMenuComponent {
     constructor(
         private readonly _router: Router,
         private readonly _bluiSecurityService: BluiAuthSecurityService,
-        private readonly _bluiChangePasswordService: BluiChangePasswordDialogService
+        private readonly _bluiChangePasswordService: BluiChangePasswordDialogService,
+        private readonly _bottomSheet: MatBottomSheet
     ) {}
 
     changePassword(): void {
         this.open = false;
+        this._bottomSheet.dismiss();
         this._bluiChangePasswordService.openDialog();
     }
 
     logout(): void {
         this._bluiSecurityService.updateSecurityState({ isAuthenticatedUser: false });
         void this._router.navigate([AUTH_ROUTES.AUTH_WORKFLOW]);
+        this._bottomSheet.dismiss();
     }
 }
